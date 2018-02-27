0877e0de7e74964acd88d08cafc341b36243e0bb
Igor Lavrentjev <Igor.Lavrentjev@bbrail.com>
2018-02-27 18:32:06 +0000

    popup for pcs

--- Content/src/components/ClientCardDescriptor.jsx
+++ Content/src/components/ClientCardDescriptor.jsx
@@ -4,14 +4,21 @@ import { List, ListItem } from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Icon } from 'semantic-ui-react'
 import PluginModal from './PluginModal.jsx'
+import Popover from 'material-ui/Popover'
+import MenuItem from 'material-ui/MenuItem'
+import Menu from 'material-ui/Menu';
 
 
 export default class ClientCardDescriptor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            open: false
+            open: false,
+            openPcs: false,
+            pcs: []
         };
+        this.handlePcToggle = this.handlePcToggle.bind(this)
+        this.handleRequestClose = this.handleRequestClose.bind(this)
     }
 
     handleToggle = () => {
@@ -20,41 +27,72 @@ export default class ClientCardDescriptor extends Component {
         });
     };
 
+    handlePcToggle = (event, pcs) => {
+        this.setState({
+            openPcs: !this.state.openPcs,
+            anchorEl: event.currentTarget,
+            pcs: pcs
+        })
+    }
+
+    handleRequestClose = () => {
+        console.log("Hello")
+        this.setState({
+            openPcs: false,
+        });
+    };
+
     render() {
         var { fleets } = this.props
+        var that = this
 
         return <MuiThemeProvider><List>
-                {fleets.map(function (fleet, i) {
-                    return <ListItem key={i}
-                        primaryText={fleet.Identity.Name}
-                        leftIcon={<Icon name='user circle' size='large' />}
-                        nestedItems={fleet.Vehicles.map(function (vehicle, i) {
-                            return <ListItem key={i}
-                                primaryText={vehicle.Identity.Name}
-                                leftIcon={<Icon name='train' size='large' />}
-                                nestedItems={vehicle.Products.map(function (product, i) {
-                                    return <ListItem key={i}
-                                        primaryText={product.Identity.Name}
-                                        leftIcon={<Icon name='archive' size='large' />}
-                                        nestedItems={product.Frameworks.map(function (framework, i) {
-                                            return <ListItem key={i}
-                                                primaryText={framework.Identity && framework.Identity.Name}
-                                                leftIcon={<Icon name='setting' size='large' />}
-                                                nestedItems={framework.StarterMotorConfigurations && framework.StarterMotorConfigurations.map(function (plugin, i) {
-                                                    return <ListItem
-                                                        key={i}
-                                                        primaryText={<PluginModal plugin={plugin} />}
-                                                        leftIcon={<Icon name='plug' size='large' />} />
-                                                })}
-                                            />
-                                        })}
-                                    />
-                                })}
-                            />
-                        })} >
-                    </ListItem>
-                })}
-        </List></MuiThemeProvider>;
+            <Popover
+                open={this.state.openPcs}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+                targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+                onRequestClose={that.handlePcToggle}
+            >
+                <Menu>
+                    {this.state.pcs && this.state.pcs.map(pc => {
+                        return <MenuItem primaryText={pc.Identity.Name} />
+                    })}
+                </Menu>
+            </Popover>
+            {fleets.map(function (fleet, i) {
+                return <ListItem key={i}
+                    primaryText={fleet.Identity.Name}
+                    leftIcon={<Icon name='user circle' size='large' />}
+                    nestedItems={fleet.Vehicles.map(function (vehicle, i) {
+                        return <ListItem key={i}
+                            primaryText={vehicle.Identity.Name}
+                            leftIcon={<Icon name='train' size='large' />}
+                            onClick={(event) => that.handlePcToggle(event, vehicle.Pcs)}
+                            nestedItems={vehicle.Products.map(function (product, i) {
+                                return <ListItem key={i}
+                                    primaryText={product.Identity.Name}
+                                    leftIcon={<Icon name='archive' size='large' />}
+                                    nestedItems={product.Frameworks.map(function (framework, i) {
+                                        return <ListItem key={i}
+                                            primaryText={framework.Identity && framework.Identity.Name}
+                                            leftIcon={<Icon name='setting' size='large' />}
+                                            nestedItems={framework.StarterMotorConfigurations && framework.StarterMotorConfigurations.map(function (plugin, i) {
+                                                return <ListItem
+                                                    key={i}
+                                                    primaryText={<PluginModal plugin={plugin} />}
+                                                    leftIcon={<Icon name='plug' size='large' />} />
+                                            })}
+                                        />
+                                    })}
+                                />
+                            })}
+                        />
+                    })} >
+                </ListItem>
+            })}
+        </List>
+        </MuiThemeProvider>;
     }
 }
 
